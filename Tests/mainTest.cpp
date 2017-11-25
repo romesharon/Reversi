54f140b257fd1e941000c802d04e45e03109c6b9
romesharon <33751805+romesharon@users.noreply.github.com>
2017-11-25 21:46:40 +0200

    Tests to classes: ReversRule, Cell and CellManager

---
+++ Tests/mainTest.cpp
@@ -0,0 +1,13 @@
+//
+// Created by rom on 25/11/17.
+//
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+
+int main(int argc, char* argv[]){
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    return 0;
+}
