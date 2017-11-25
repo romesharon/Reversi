48936536367a288449a9588476ea1af2da6bfc57
aviv0102 <33751941+aviv0102@users.noreply.github.com>
2017-11-25 15:49:54 +0200

    Add files via upload
    new tests

---
+++ Gtest_main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by magshimim on 25/11/17.
+//
+
+#include <gtest/gtest.h>
+
+
+GTEST_API_ int main(int argc,char** argv){
+    testing::InitGoogleTest(&argc,argv);
+
+    return RUN_ALL_TESTS();
+
+
+}
---
+++ HumanTest.cpp
@@ -0,0 +1,26 @@
+//aviv shisman 206558157
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../HumanPlayer.h"
+
+
+TEST(HumanPlayerTest,testSymbol){
+    HumanPlayer p1('x');
+    HumanPlayer p2('o');
+    ASSERT_EQ('x',p1.getSymbol());
+    ASSERT_EQ('o',p2.getSymbol());
+    EXPECT_FALSE(p1.getSymbol()=='o');
+    EXPECT_FALSE(p1.getSymbol()!='x');
+
+}
+//roie said we cant check the interface with user?
+TEST(HumanPlayert__Test,testOneMove){
+    Point** points=new Point*[1];
+    points[0]=new Point(1,1);
+    HumanPlayer p1('x');
+    Point* p2=p1.oneMove(points,1);
+    EXPECT_EQ(p2->getY(),points[0]->getY());
+    EXPECT_EQ(p2->getX(),points[0]->getX());
+    delete(points[0]);
+    delete(points);
+}
